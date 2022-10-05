0b751108bd3b371ce42e00f7c970bf70242f5f59
Antony Botello <antonybotello@gmail.com>
2022-10-04 21:26:28 -0500

    base de proyecto lista

--- base/urls.py
+++ base/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from base.views import inicioAdmin
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('adm/',inicioAdmin,name='inicio-admin'),
