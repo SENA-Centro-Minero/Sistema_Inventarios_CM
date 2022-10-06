6e8cb8f864e1f58324ddde7142b33720acc10df4
Inés María O <inesmao@misena.edu.co>
2022-10-05 20:55:11 -0500

    Update models.py

--- facturas/models.py
+++ facturas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator
+
 
 # Create your models here.
+class Factura(models.Model):
+    fecha=models.DateField(verbose_name="Fecha Factura", help_text=u"MM/DD/AAAA")
